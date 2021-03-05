c5f87f3d4abe2dca4cc5ed3c99a6fff1cabc5db4
Miura Toya <dengzais8@gmail.com>
2021-03-05 14:57:36 +0900

    Initial Commit

---
+++ SampleCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleCollectionView
+//
+//  Created by 三浦　登哉 on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
